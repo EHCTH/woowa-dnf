e1fc16e6be35a07ad3b42e1977d44039aa036cb5
EHCTH <lees90896@gmail.com>
2025-11-18 14:51:04 +0900

    chore(app): 부트 메인 추가

---
+++ src/main/java/xyz/woowa/dnf/DnfWebApplication.java
@@ -0,0 +1,12 @@
+package xyz.woowa.dnf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnfWebApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DnfWebApplication.class, args);
+	}
+
+}
