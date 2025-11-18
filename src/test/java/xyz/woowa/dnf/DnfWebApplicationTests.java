0aebf99ad65884ade3f0efd02bd67da745cfb0c3
EHCTH <lees90896@gmail.com>
2025-11-18 14:53:41 +0900

    test(app): 부트 메인 테스트 추가

---
+++ src/test/java/xyz/woowa/dnf/DnfWebApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.woowa.dnf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnfWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
