4c2955d041038bff5c4d749946dcc4cba5f7d80d
EHCTH <lees90896@gmail.com>
2025-11-18 14:35:16 +0900

    feat(character): 캐릭터 조회/갱신 파사드 구현

---
+++ src/main/java/xyz/woowa/dnf/character/application/command/service/CharacterRefreshFacade.java
@@ -0,0 +1,79 @@
+package xyz.woowa.dnf.character.application.command.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import xyz.woowa.dnf.character.application.command.assembler.CharacterAssembler;
+import xyz.woowa.dnf.character.application.command.port.inbound.SaveCharacterUseCase;
+import xyz.woowa.dnf.character.application.query.dto.CharacterDto;
+import xyz.woowa.dnf.character.application.query.port.inbound.GetCharacterDetailUseCase;
+import xyz.woowa.dnf.character.domain.Character;
+import xyz.woowa.dnf.character.domain.base.Base;
+import xyz.woowa.dnf.character.domain.base.EntityId;
+import xyz.woowa.dnf.character.domain.base.Server;
+import xyz.woowa.dnf.common.aop.CharacterLog;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class CharacterRefreshFacade {
+
+    private final GetCharacterDetailUseCase query;
+    private final CharacterAssembler assembler;
+    private final SaveCharacterUseCase saver;
+    private final CharacterParallelLoader loader;
+
+    @Transactional
+    @CharacterLog("캐릭터 조회")
+    public CharacterDto character(String serverId, String characterId) {
+        EntityId entityId = new EntityId(characterId, Server.fromEnglish(serverId));
+        var command = command(serverId, characterId);
+        Character exist = saver.findByEntityId(entityId);
+        if (hasCharacter(exist)) {
+            return refreshIfChanged(exist, entityId, command);
+        }
+        return loadAndSaveNewCharacter(entityId, command);
+    }
+
+    @Transactional
+    @CharacterLog("캐릭터 강제 갱신")
+    public CharacterDto refresh(String serverId, String characterId) {
+        EntityId entityId = new EntityId(characterId, Server.fromEnglish(serverId));
+        log.info("캐릭터 정보 갱신 시작 = {}",entityId);
+        var command = command(serverId, characterId);
+        Base base = query.get(command);
+        return refresh(entityId, command, base);
+    }
+
+    private GetCharacterDetailUseCase.Command command(String serverId, String characterId) {
+        return new GetCharacterDetailUseCase.Command(serverId, characterId);
+    }
+
+    private boolean hasCharacter(Character character) {
+        return character != null;
+    }
+
+    private CharacterDto refreshIfChanged(Character exist,
+                                          EntityId entityId,
+                                          GetCharacterDetailUseCase.Command command) {
+        Base base = query.get(command);
+        if (exist.equalsToBase(base)) {
+            return assembler.toCharacterDto(exist);
+        }
+        return refresh(entityId, command, base);
+    }
+
+    private CharacterDto refresh(EntityId entityId, GetCharacterDetailUseCase.Command command, Base base) {
+        Character refreshed = loader.refresh(base, entityId, command);
+        saver.update(refreshed);
+        return assembler.toCharacterDto(refreshed);
+    }
+
+    private CharacterDto loadAndSaveNewCharacter(EntityId entityId,
+                                                 GetCharacterDetailUseCase.Command command) {
+        Character character = loader.load(entityId, command);
+        saver.save(character);
+        return assembler.toCharacterDto(character);
+    }
+}
