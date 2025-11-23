80da2d095b3d87a8752f61efa60b4846ea92782d
EHCTH <lees90896@gmail.com>
2025-11-23 11:46:50 +0900

    refactor(chat): 트랜잭션 에너테이션 중복 제거

--- src/main/java/xyz/woowa/dnf/character/application/command/service/SaveCharacterService.java
+++ src/main/java/xyz/woowa/dnf/character/application/command/service/SaveCharacterService.java
@@ -2,14 +2,12 @@ package xyz.woowa.dnf.character.application.command.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import xyz.woowa.dnf.character.application.command.port.inbound.SaveCharacterUseCase;
 import xyz.woowa.dnf.character.application.command.port.outbound.CharacterRepository;
 import xyz.woowa.dnf.character.domain.Character;
 import xyz.woowa.dnf.character.domain.base.EntityId;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 public class SaveCharacterService implements SaveCharacterUseCase {
     private final CharacterRepository characterRepository;
@@ -20,7 +18,6 @@ public class SaveCharacterService implements SaveCharacterUseCase {
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Character findByEntityId(EntityId id) {
         return characterRepository.findById(id);
     }
